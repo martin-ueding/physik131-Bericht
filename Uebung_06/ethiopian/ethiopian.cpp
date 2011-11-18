a29bd4d6214cb3626e0581af241eb23c03b0eb99
Martin Ueding <dev@martin-ueding.de>
2011-11-18 12:46:53 +0100

    Add makefile, pretty print

--- Uebung_06/ethiopian/ethiopian.cpp
+++ Uebung_06/ethiopian/ethiopian.cpp
@@ -1,15 +1,25 @@
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		std::cout << "Usage: ethiopian NUMBER NUMBER" << std::endl;
+		return 1;
+	}
+
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 
 	int res = 0;
+
 	while (a >= 1) {
-		std::cout << a << "  " << b << std::endl;
+		printf("%6d %6d\n", a, b);
+
 		if (a % 2 == 1) {
 			res += b;
 		}
+
 		a /= 2;
 		b *= 2;
 	}
