4fdec097cfc89fb6c27254b7afe5293b1dae2187
Martin Ueding <dev@martin-ueding.de>
2011-11-18 12:39:38 +0100

    Astyle

--- Uebung_06/ethiopian/ethiopian.cpp
+++ Uebung_06/ethiopian/ethiopian.cpp
@@ -4,14 +4,14 @@ int main(int argc, char *argv[]) {
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 
-	int res=0;
+	int res = 0;
 	while (a >= 1) {
 		std::cout << a << "  " << b << std::endl;
-		if (a%2==1) {
-		res += b;
+		if (a % 2 == 1) {
+			res += b;
 		}
-		a/=2;
-		b*=2;
+		a /= 2;
+		b *= 2;
 	}
 	std::cout << "Ergebnis: " << res << std::endl;
 
