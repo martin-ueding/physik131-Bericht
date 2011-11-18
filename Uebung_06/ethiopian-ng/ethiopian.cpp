e8aa7f45d2e0fd495069d3828e314aa234d32f7b
Martin Ueding <dev@martin-ueding.de>
2011-11-18 13:07:22 +0100

    Create a loop

--- Uebung_06/ethiopian-ng/ethiopian.cpp
+++ Uebung_06/ethiopian-ng/ethiopian.cpp
@@ -3,27 +3,29 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		std::cout << "Usage: ethiopian NUMBER NUMBER" << std::endl;
-		return 1;
-	}
+	int a;
+	int b;
+	int res;
 
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	for (a = 0; a <= 10; a++) {
+		for (b = a; b <= 10; b++) {
+			printf("Calculating %d times %d: \n", a, b);
+			res = 0;
 
-	int res = 0;
+			while (a >= 1) {
+				printf("-> %6d %6d\n", a, b);
 
-	while (a >= 1) {
-		printf("%6d %6d\n", a, b);
+				if (a % 2 == 1) {
+					res += b;
+				}
 
-		if (a % 2 == 1) {
-			res += b;
+				a /= 2;
+				b *= 2;
+			}
+			std::cout << "Result: " << res << std::endl;
+			std::cout << std::endl;
 		}
 
-		a /= 2;
-		b *= 2;
+		return 0;
 	}
-	std::cout << "Result: " << res << std::endl;
-
-	return 0;
 }
