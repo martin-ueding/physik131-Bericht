e92272bb49f52c0819f1edc5f1132fd8d7484864
Martin Ueding <dev@martin-ueding.de>
2011-11-25 20:12:02 +0100

    Add multiplication function

--- Uebung_07/arrays/arrays.cpp
+++ Uebung_07/arrays/arrays.cpp
@@ -1,21 +1,29 @@
 // Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
 
-#include <iostream>
+#include <cstdio>
 #include <fstream>
+#include <iostream>
 
 #define LENGTH 50
+
+double mult(double a, double b) {
+	return a * b;
+}
 
 int main() {
 	std::ifstream infile;
 	infile.open("data2.dat");
 
 	double x[LENGTH], y[LENGTH];
+	double mul[LENGTH], div[LENGTH], add[LENGTH];
 
 	for (int n = 0; n < LENGTH && infile.good(); n++) {
 		infile >> x[n] >> y[n];
-		if (!infile.good())
+		if (!infile.good()) {
 			break;
-		std::cout << x[n] << y[n] << std::endl;
+		}
+
+		printf("%10.3f * %10.3f = %10.3f\n", x[n], y[n], mult(x[n], y[n]));
 	}
 
 	return 0;
