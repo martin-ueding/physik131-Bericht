7903021eeecab8543b0bb7462ce648ce0029122d
Martin Ueding <dev@martin-ueding.de>
2011-12-02 13:10:53 +0100

    Clean up output

--- Uebung_08/Student/main.cpp
+++ Uebung_08/Student/main.cpp
@@ -8,27 +8,19 @@ int main(void) {
 	Student studi;
 	string temp_vorname;
 
-	cout << "\nHier wird der Student getestet!" << endl;
 	cout << "Default Voname und Nachname  des Studenten: " << studi.getVorname() << " " <<  studi.getNachname() << endl;
 
-	cout << "--------------------------------" << endl;
-	cout << "Nun werden neue Paramter des Studenten gesetzt." << endl;
-	cout << "--------------------------------" << endl;
 	cout << "Setze einen Vornamen: ";
 	cin >> temp_vorname;
 	studi.setVorname(temp_vorname);
 	cout << endl << "Neuer Voname des Studenten: " << studi.getVorname() << endl;
 
-	cout << "--------------------------------" << endl;
 	int tag, monat, jahr;
 	cout << "Hier bitte die Geburtsdaten im Format <Tag Monat Jahr> eingeben (ab 1970)." << endl;
 	cin >> tag >> monat >> jahr;
-	cout << "\nDas Geburtsdatum lautet: " << tag << " " << monat << " " << jahr << endl;
+	cout << "Das Geburtsdatum lautet: " << tag << " " << monat << " " << jahr << endl;
 	studi.setGeburtstag(tag, monat, jahr);
 	cout <<  "Das Alter des Studenten ist: " << studi.getAlter() << endl;
-	cout << "--------------------------------" << endl;
-
-	cout << endl << "...beende main...." << endl;
 
 	return 0;
 }
