3849091d3929a68c2ca0481a9fc4f64904e1be32
Martin Ueding <dev@martin-ueding.de>
2011-12-02 13:03:26 +0100

    Delete pointless comments

--- Uebung_08/Student/main.cpp
+++ Uebung_08/Student/main.cpp
@@ -1,8 +1,3 @@
-/* Datei main.cpp
-   In dieser Datei wird eine Beispielanwendung der Klasse Student implementiert.
- */
-
-// binde die Beschreibung von Student ein
 #include "student.h"
 #include <iostream>
 #include <string>
@@ -10,22 +5,17 @@
 using namespace std;
 
 int main(void) {
-	// Lege ein Studenten  mit dem Standard-Konstruktor an.
 	Student studi;
-	// Diesen String benötigen wir um den Vornamen zu übergeben
 	string temp_vorname;
 
 	cout << "\nHier wird der Student getestet!" << endl;
-	// teste die Methoden der Instanz a von Student
 	cout << "Default Voname und Nachname  des Studenten: " << studi.getVorname() << " " <<  studi.getNachname() << endl;
 
 	cout << "--------------------------------" << endl;
 	cout << "Nun werden neue Paramter des Studenten gesetzt." << endl;
 	cout << "--------------------------------" << endl;
 	cout << "Setze einen Vornamen: ";
-	// Eingabe des neuen vornamens und temporär speichern in temp_vorname
 	cin >> temp_vorname;
-	// Setzt den Namen das angelegten Studenten (Objekt std)
 	studi.setVorname(temp_vorname);
 	cout << endl << "Neuer Voname des Studenten: " << studi.getVorname() << endl;
 
@@ -40,6 +30,5 @@ int main(void) {
 
 	cout << endl << "...beende main...." << endl;
 
-	// Rückgabe 0 an das Betriebssystem soll heißen, erfolgreich beendet
 	return 0;
 }
--- Uebung_08/Student/student.h
+++ Uebung_08/Student/student.h
@@ -1,60 +1,33 @@
-/* Datei student.h
-  In dieser Datei befindet sich die Beschreibung/Deklaration der Klasse.
-*/
-
-/* Damit eine mehrfache Deklaration der Klasse durch mehrfaches Einbinden
-   dieser Headerdatei verhindert wird, wird über die Präprozessor-Anweisung
-   "#define AUTO_H" ein Wert gesetzt, den man mittels "ifdef" bzw.
-   "ifndef" abprüfen kann. Dieser gibt Auskunft, ob diese Headerdatei bereits
-   eingebunden wurde.
-*/
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include <iostream>	// für cout
-#include <string>	// für string Operationen
-#include <time.h>       // für datums und Zeitinforationen
+#include <iostream>
+#include <string>
+#include <time.h>
+
 using namespace std;
 
-/*
-   Hier folgt die Beschreibung der Klasse Student
-*/
 class Student {
-// privater Teil der Klasse (darauf hat nur die Klasse Zugriff)
 private:
-	/* Eine Eigenschaft/ein Attribut einer Realisierung/Instanz von Student,
-	   welche den Vornamen beinhaltet.
-	 */
 	string vorname;
-	/* Eine weitere Eigenschaft, die den Nachnamen darstellt.
-	 */
 	string nachname;
 
-	/* Die Eigenschaft Geburtsdatum */
 	int geb_tag;
 	int geb_monat;
 	int geb_jahr;
 
-	// öffentlicher Teil der Klasse (alles darunter angegebene kann von
-	// jedem verwendet werden)
 public:
-	/* Ein Konstruktor zum Generieren eines Student-Objektes */
 	Student();
-	/* Ein Destruktor zum Zerstören eines Student-Objektes */
 	~Student();
 
-	/* Eine Methode, mit der man den Vornamen des Studenten setzen kann */
 	void setVorname(string vorname);
-	/* Eine Methode, mit der man den Vornamen des Studenten auslesen kann */
 	string getVorname();
-	/* analog Voname,hier der Nachname*/
 	void setNachname(string nachname);
 	string getNachname();
 
-	/* Eine Methode um das Alter des Studenten festzustellen*/
 	int getAlter();
 	void setGeburtstag(int geb_tag, int geb_monat, int geb_jahr);
 
-}; // Dieses Semikolon darf nicht fehlen!!!
+};
 
 #endif
