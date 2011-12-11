79e31fa9e7b916afebd14e020d03cae469d74bf9
Martin Ueding <dev@martin-ueding.de>
2011-12-11 14:24:45 +0100

    Fix errors and warnings

--- Uebung_09/array-pointer/pointer.cpp
+++ Uebung_09/array-pointer/pointer.cpp
@@ -8,17 +8,17 @@ int main() {
 	int array[10];
 
 	pointer = array;
-	array = pointer;
+	// array = pointer;
 	pointer = &array[3];
 	array[3] = pointer[5];
 
-	int *p1, *p2, i;
+	int *p1 = (int*) 0xDEADBEEF, *p2 = (int*) 0xDEADBEEF, i = 0;
 
 	p1 = p2 + i;
 	p1 = i + p2;
-	i = p1 * p2;
+	// i = p1 * p2;
 	i = p1 - p2;
-	i = p1 + p2;
+	// i = p1 + p2;
 
 	return 0;
 }
