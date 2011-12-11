5a051275b0f71653669513b6e060eab126bc2d0e
Martin Ueding <dev@martin-ueding.de>
2011-12-11 15:51:14 +0100

    Shorten output

--- Uebung_06/ethiopian-ng/ethiopian.cpp
+++ Uebung_06/ethiopian-ng/ethiopian.cpp
@@ -9,16 +9,14 @@ int main(int argc, char *argv[]) {
 
 	int i, j;
 	for (i = 1; i <= 10; i++) {
+		printf("%2d:", i);
 		for (j = i; j <= 10; j++) {
 			a = i;
 			b = j;
 			res = 0;
 
-			printf("Calculating %d times %d: \n", a, b);
 
 			while (a >= 1) {
-				printf("-> %6d %6d\n", a, b);
-
 				if (a % 2 == 1) {
 					res += b;
 				}
@@ -26,9 +24,9 @@ int main(int argc, char *argv[]) {
 				a /= 2;
 				b *= 2;
 			}
-			std::cout << "Result: " << res << std::endl;
-			std::cout << std::endl;
+			printf(" %3d", res);
 		}
+		std::cout << std::endl;
 	}
 
 		return 0;
