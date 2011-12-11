ab5ea3d0572cdf43050bcd3dd7327a0be4ba0ca3
Martin Ueding <dev@martin-ueding.de>
2011-12-11 15:57:31 +0100

    80 cols

--- Uebung_08/Student/main.cpp
+++ Uebung_08/Student/main.cpp
@@ -8,27 +8,34 @@ int main(void) {
 	Student student;
 	string temp_vorname;
 
-	cout << "Standardvorname und -nachname des Studenten: " << student.getVorname() << " " <<  student.getNachname() << endl;
+	cout << "Standardvorname und -nachname des Studenten: "
+	     << student.getVorname() << " " <<  student.getNachname() << endl;
 
 	cout << "Setze einen Vornamen: ";
 	cin >> temp_vorname;
 	student.setVorname(temp_vorname);
-	cout << endl << "Neuer Voname des Studenten: " << student.getVorname() << endl;
+	cout << endl << "Neuer Voname des Studenten: " << student.getVorname()
+	     << endl;
 
 	cout << "Setze einen Nachnamen: ";
 	cin >> temp_vorname;
 	student.setNachname(temp_vorname);
-	cout << endl << "Neuer Nachnamen des Studenten: " << student.getNachname() << endl;
+	cout << endl << "Neuer Nachnamen des Studenten: " << student.getNachname()
+	     << endl;
 
-	cout << "Neuer Name des Studenten: " << student.getVorname() << " " <<  student.getNachname() << endl;
+	cout << "Neuer Name des Studenten: " << student.getVorname() << " "
+	     << student.getNachname() << endl;
 
 	int tag, monat, jahr;
-	cout << "Hier bitte die Geburtsdaten im Format <Tag Monat Jahr> eingeben (ab 1970)." << endl;
+	cout << "Hier bitte die Geburtsdaten im Format <Tag Monat Jahr> eingeben (ab 1970)."
+	     << endl;
 	cin >> tag >> monat >> jahr;
-	cout << "Das Geburtsdatum lautet: " << tag << " " << monat << " " << jahr << endl;
+	cout << "Das Geburtsdatum lautet: " << tag << " " << monat << " " << jahr
+	     << endl;
 	student.setGeburtstag(tag, monat, jahr);
 	cout <<  "Das Alter des Studenten ist: " << student.getAlter() << endl;
-	cout <<  "Der Geburtstag des Studenten ist (YYYYMMDD): " << student.getGeburtstag() << endl;
+	cout <<  "Der Geburtstag des Studenten ist (YYYYMMDD): "
+	     << student.getGeburtstag() << endl;
 
 	return 0;
 }
