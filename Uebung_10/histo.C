544f8440de4f878597255a41f334922396ff6be2
Martin Ueding <dev@martin-ueding.de>
2011-12-23 21:56:43 +0100

    More comments

--- Uebung_10/histo.C
+++ Uebung_10/histo.C
@@ -1,11 +1,23 @@
+/**
+ * Creates a histogram with given data.
+ */
 {
+	// Create a new canvas.
 	TCanvas *c1 = new TCanvas("c1", "mein canvas", 800, 500);
+
+	// Create a new histogram.
 	TH1F *h1 = new TH1F("h1", "mein histo", 10, 0, 10);
+
+	// Fill the latter with data.
 	h1->Fill(3.2);
 	h1->Fill(7);
 	h1->Fill(3.6);
 	h1->Fill(4.2);
+
+	// Label the axes.
 	h1->SetXTitle("x");
 	h1->SetYTitle("Anzahl");
+
+	// Draw it.
 	h1->Draw();
 }
