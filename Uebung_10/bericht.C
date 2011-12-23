1fcae19aacbb147b25c46a8b3ec9c86970119a25
Martin Ueding <dev@martin-ueding.de>
2011-12-23 19:39:48 +0100

    More comments

--- Uebung_10/bericht.C
+++ Uebung_10/bericht.C
@@ -1,11 +1,29 @@
+/**
+ * ROOT macro to parse voltage and current.
+ */
 {
+	// Generate a new Canvas element with the given size.
 	TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+
+	// Change to the first part of this.
 	c1->cd(1);
+
+	// Create a new graph with error bars from the data file.
 	TGraphErrors *tge = new TGraphErrors("data_fehler_bericht_.dat");
+
+	// Draw the graph with axes and points.
 	tge->Draw("ap");
+
+	// Apply a linear fit, first order polynomial.
 	tge->Fit("pol1");
+
+	// Label the axes.
 	tge->GetXaxis()->SetTitle("Spannung/[V]");
 	tge->GetYaxis()->SetTitle("Strom/[A]");
+
+	// Update the canvas to accommodate the changes of the plot.
 	c1->Update();
+
+	// Save the whole canvas to a PDF file.
 	c1->Print("histogram.pdf");
 }
