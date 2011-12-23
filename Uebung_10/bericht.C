2432be1a3883a2ed30db0f3a6ae6b881a9602178
Martin Ueding <dev@martin-ueding.de>
2011-12-23 19:18:02 +0100

    Export to PDF

--- Uebung_10/bericht.C
+++ Uebung_10/bericht.C
@@ -1,5 +1,11 @@
 {
+	TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+	c1->cd(1);
 	TGraphErrors *tge = new TGraphErrors("data_fehler_bericht_.dat");
 	tge->Draw("ap");
 	tge->Fit("pol1");
+	tge->GetXaxis()->SetTitle("Spannung/[V]");
+	tge->GetYaxis()->SetTitle("Strom/[A]");
+	c1->Update();
+	c1->Print("histogram.pdf");
 }
